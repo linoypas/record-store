e4874f7391323d2d83d6834f70c590bda85c8672
inbar <inbar2268@gmail.com>
2024-09-10 09:20:25 +0300

    add statistics page

--- services/graphs.js
+++ services/graphs.js
@@ -1,11 +1,26 @@
 const productService = require('./product');
+const product = require('../models/product');
 
 
 async function GenresGraph(){
-    genres = productService.getListOfGenres();
-    genres.forEach(element => {
-        console.log(element)
-    });
+    genres = await productService.getListOfGenres();
+    genresData = {};
+
+    await Promise.all(genres.map(async (genre) => {
+        const number = await getNumOfProductsGenre(genre);
+        genresData[genre] = number;
+      }));
+    return genresData;
+}
+
+
+async function getNumOfProductsGenre(genre) {
+    try{
+        numberOfProducts = await product.find({genre: genre});
+        return numberOfProducts.length;
+    } catch{
+        return false;
+    } 
 }
 
 module.exports = {
