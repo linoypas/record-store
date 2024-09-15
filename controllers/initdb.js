330b6095c832926b3d4aead5c7b99f07ae18c0d5
inbar <inbar2268@gmail.com>
2024-09-15 16:39:17 +0300

    add data to initdb function

--- controllers/initdb.js
+++ controllers/initdb.js
@@ -25,22 +25,29 @@ async function updateProductCollection(req,res){
         await productService.createProduct("Pop",2024,"Sabrina Carpenter","Short N’ Sweet",120,"תכניסו כאן תיאור",{buffer: fs.readFileSync(imagePath),mimetype: path.extname(imagePath).slice(1)},true);
         
         imagePath = path.join(__dirname, '../database/images/Post-Malone-Twelve-Carat-Toothache.webp');
-        await productService.createProduct("Hip Hop",2022,"Post Malone","Twelve Carat Toothache",80,"תכניסו כאן תיאור",{buffer: fs.readFileSync(imagePath),mimetype: path.extname(imagePath).slice(1)},false);
+        await productService.createProduct("Hip Hop",2022,"Post Malone","Twelve Carat Toothache",80,"תכניסו כאן תיאור",{buffer: fs.readFileSync(imagePath),mimetype: path.extname(imagePath).slice(1)},true);
         
         imagePath = path.join(__dirname, '../database/images/Drake-Honestly-Nevermind.webp');
         await productService.createProduct("Hip Hop",2023,"Drake","Honestly, Nevermind",110,"תכניסו כאן תיאור",{buffer: fs.readFileSync(imagePath),mimetype: path.extname(imagePath).slice(1)},false);
         
         imagePath = path.join(__dirname, '../database/images/scorpion-drake.jpg');
-        await productService.createProduct("Hip Hop",2018,"Drake","Scorpion",130,"תכניסו כאן תיאור",{buffer: fs.readFileSync(imagePath),mimetype: path.extname(imagePath).slice(1)},false);
+        await productService.createProduct("Hip Hop",2018,"Drake","Scorpion",130,"תכניסו כאן תיאור",{buffer: fs.readFileSync(imagePath),mimetype: path.extname(imagePath).slice(1)},true);
         
         imagePath = path.join(__dirname, '../database/images/Train_–_Greatest_Hits.png');
-        await productService.createProduct("Rock",2018,"Train","Greatest Hits",130,"תכניסו כאן תיאור",{buffer: fs.readFileSync(imagePath),mimetype: path.extname(imagePath).slice(1)},false);
+        await productService.createProduct("Rock",2018,"Train","Greatest Hits",130,"תכניסו כאן תיאור",{buffer: fs.readFileSync(imagePath),mimetype: path.extname(imagePath).slice(1)},true);
         
         imagePath = path.join(__dirname, '../database/images/one-more-time-blink182.jpg');
-        await productService.createProduct("Rock",2023,"Blink 182","One More Time... part 2",130,"תכניסו כאן תיאור",{buffer: fs.readFileSync(imagePath),mimetype: path.extname(imagePath).slice(1)},false);
+        await productService.createProduct("Rock",2023,"Blink 182","One More Time... part 2",130,"תכניסו כאן תיאור",{buffer: fs.readFileSync(imagePath),mimetype: path.extname(imagePath).slice(1)},true);
         
         imagePath = path.join(__dirname, '../database/images/shalom-hanoch-mehakim-lamechiah.jpg');
-        await productService.createProduct("ישראליים",1985 ,"שלום חנוך","מחכים למשיח",135,"תכניסו כאן תיאור",{buffer: fs.readFileSync(imagePath),mimetype: path.extname(imagePath).slice(1)},false);
+        await productService.createProduct("ישראליים",1985 ,"שלום חנוך","מחכים למשיח",135,"תכניסו כאן תיאור",{buffer: fs.readFileSync(imagePath),mimetype: path.extname(imagePath).slice(1)},true);
+        
+        imagePath = path.join(__dirname, '../database/images/caveret-zafuf-baozen.jpg');
+        await productService.createProduct("ישראליים",1975 ,"כוורת","צפוף באוזן",105,"תכניסו כאן תיאור",{buffer: fs.readFileSync(imagePath),mimetype: path.extname(imagePath).slice(1)},false);
+        
+        imagePath = path.join(__dirname, '../database/images/Kaveret_Sipori.jpg');
+        await productService.createProduct("ישראליים",1973 ,"כוורת","סיפורי פוגי",90,"תכניסו כאן תיאור",{buffer: fs.readFileSync(imagePath),mimetype: path.extname(imagePath).slice(1)},true);
+
         
         
         res.status(200).render('../views/homePage')
