f5eb9c8df80d8c74bbf9249abc0a45c6fb90343b
נועה בן ציון <noabenzion@h-MBP-sl-nwh.lan>
2024-11-08 21:27:08 +0200

    add shopping cart

--- routes/orders.js
+++ routes/orders.js
@@ -8,7 +8,7 @@ router.get('/order/:id', orderController.getorder);
 router.put('/order/:id', orderController.updateorder)
 router.delete('/order/:id', orderController.deleteorder)
 //router.get('/orders/cart/:id',orderController.showcart);
-router.put('/order/cart/:id', orderController.updatecart)
+router.put('/order/cart/:id', orderController.updatecart);
 //router.post('/order/:id', orderController.createorder)
 
 module.exports = router;
