bb7849ead7996894bce440eb2f4a2cf0089ad240
נועה בן ציון <noabenzion@h-MacBook-Pro-sl-nwh.local>
2024-11-10 16:06:14 +0200

    add store function and css

--- controllers/storeLocationController.js
+++ controllers/storeLocationController.js
@@ -18,7 +18,8 @@ exports.getAllStoreLocations = async (req, res) => {
 
 exports.addStoreLocation = async (req, res) => {
     const { name, address, latitude, longitude } = req.body;
-
+    const isAdmin = req.session.isAdmin || false;
+    const username = req.session.username || 'Guest'; 
     const storeLocation = new StoreLocation({
         name,
         address,
@@ -28,23 +29,13 @@ exports.addStoreLocation = async (req, res) => {
 
     try {
         const savedStoreLocation = await storeLocation.save();
-        res.status(201).json(savedStoreLocation);
+        res.render('storeLocation', {
+            storeLocations: await StoreLocation.find(),
+            isAdmin: isAdmin,
+            username: username,
+            message:'Store location created successfully!'
+        });
     } catch (err) {
         res.status(400).json({ message: err.message });
-    }
-};
-
-exports.deleteStoreLocation = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deletedLocation = await StoreLocation.findByIdAndDelete(id);
-        
-        if (!deletedLocation) {
-            return res.status(404).json({ message: 'Store location not found' });
-        }
-        
-        res.status(200).json({ message: 'Store location deleted successfully' });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
     }
 };
--- routes/storeLocation.js
+++ routes/storeLocation.js
@@ -4,6 +4,5 @@ const storeLocationController = require('../controllers/storeLocationController'
 
 router.get('/store-locations', storeLocationController.getAllStoreLocations);
 router.post('/store-locations', storeLocationController.addStoreLocation);
-router.delete('/store-locations/:id', storeLocationController.deleteStoreLocation);
 
 module.exports = router;
