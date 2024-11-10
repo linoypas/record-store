bc4c21acf47c72d588d25034f2c125ce05b39b37
נועה בן ציון <noabenzion@h-MacBook-Pro-sl-nwh.local>
2024-11-10 14:57:53 +0200

    add store function

--- controllers/storeLocationController.js
+++ controllers/storeLocationController.js
@@ -3,7 +3,14 @@ const StoreLocation = require('../models/storeLocation');
 exports.getAllStoreLocations = async (req, res) => {
     try {
         const storeLocations = await StoreLocation.find();
-        res.json(storeLocations);
+        const isAdmin = req.session.isAdmin || false;
+        const username = req.session.username || 'Guest'; 
+
+        res.render('storeLocation', {
+            storeLocations: storeLocations,
+            isAdmin: isAdmin,
+            username: username
+        });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
@@ -24,5 +31,20 @@ exports.addStoreLocation = async (req, res) => {
         res.status(201).json(savedStoreLocation);
     } catch (err) {
         res.status(400).json({ message: err.message });
+    }
+};
+
+exports.deleteStoreLocation = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedLocation = await StoreLocation.findByIdAndDelete(id);
+        
+        if (!deletedLocation) {
+            return res.status(404).json({ message: 'Store location not found' });
+        }
+        
+        res.status(200).json({ message: 'Store location deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 };
--- routes/storeLocation.js
+++ routes/storeLocation.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const storeLocationController = require('../controllers/storeLocationController');
 
 router.get('/store-locations', storeLocationController.getAllStoreLocations);
-
 router.post('/store-locations', storeLocationController.addStoreLocation);
+router.delete('/store-locations/:id', storeLocationController.deleteStoreLocation);
 
 module.exports = router;
